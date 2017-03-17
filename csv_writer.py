92f2ebbc2a8ccf4a6a24f5f1ba6637d18d403fbf
Paul Ambrose <pambrose@mac.com>
2017-03-17 10:00:16 -0700

    Arg cleanup

--- csv_writer.py
+++ csv_writer.py
@@ -5,12 +5,12 @@ from distance_client import Distances
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url", dest="url", required=True, help="Distance server URL")
-    parser.add_argument("-f", "--file", dest="file", required=True, help="CSV file name")
+    parser.add_argument("-f", "--file", dest="file", required=True, help="CSV filename")
     args = vars(parser.parse_args())
 
     distances = Distances(args["url"])
 
-    with  open(args["file"], 'w') as file:
+    with open(args["file"], 'w') as file:
         for val in distances.values():
             print("Wrote: {0}, {1}".format(val.elapsed, val.distance))
             file.write("{0}, {1}\n".format(val.elapsed, val.distance))
