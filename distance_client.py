5f5eff297e890da76c0f8e2d188dec33a50ea7ba
Paul Ambrose <pambrose@mac.com>
2017-03-24 13:59:26 -0700

    Add tests with threads

--- distance_client.py
+++ distance_client.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import argparse
 import logging
 import socket
 import time
@@ -42,30 +41,19 @@ class DistanceClient(SingleValueClient):
 if __name__ == "__main__":
     setup_logging()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--repeat", type=int, default=10, help="Repeat count")
-    parser.add_argument("--size", type=int, default=10, help="Sample size")
-    parser.add_argument("--print", default=False, action="store_true", help="Print values")
-    args = vars(parser.parse_args())
-
-    repeat = args["repeat"]
-    size = args["size"]
-    print_vals = args["print"]
     cnt = 0
 
-    for i in range(repeat):
+    for i in range(5):
         print("Iteration {0}".format(i))
         with DistanceClient("localhost") as client:
-            for d, j in zip(client.values(), range(size)):
-                if print_vals:
-                    print(d)
+            for d, j in zip(client.values(), range(10)):
+                print(d)
                 cnt += 1
 
-            for j in range(size):
-                if print_vals:
-                    print(client.value())
+            for j in range(10):
+                print(client.value())
                 cnt += 1
 
-    assert (cnt == repeat * size * 2)
+    assert (cnt == 5 * 10 * 2)
 
     print("Exiting...")
